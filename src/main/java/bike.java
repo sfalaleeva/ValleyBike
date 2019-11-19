800fb444841a76d0c2027104ada781e2e82381b1
Lauren <Lauren@131.229.116.96>
2019-11-19 13:04:49 -0500

    Javadoc commenting for bike class

--- src/main/java/bike.java
+++ src/main/java/bike.java
@@ -1,19 +1,28 @@
 
 public class bike {
 	
-	//ID of the current bike object
+	/**
+	 * Unique integer ID of the bike object
+	 */
 	private int ID;
-	
-	//ID of the last station it was at
+	/**
+	 * ID of the last station it was at
+	 */
 	private int statID;
 	
-	//Whether or not the bike is out on a ride
+	/**
+	 * Whether or not the bike is out on a ride
+	 */
 	private boolean onRide;
 	
-	//Whether or not the bike needs maintenance
+	/**
+	 * Whether or not the bike needs maintenance
+	 */
 	private boolean needsMaintenance;
 	
-	//Next bike ID that will be assigned for the whole class: Always 1 more than current highest id
+	/**
+	 * Next bike ID that will be assigned for the whole class: Always 1 more than current highest id
+	 */
 	static int nextBikeID;
 	
 	public bike() {
@@ -25,18 +34,36 @@ public class bike {
 	 * Accessor Methods
 	 */
 
+	/**
+	 * Gets the bike ID
+	 * @return - bike ID
+	 */
 	public int getID() {
 		return ID;
 	}
 	
+	
+	/**
+	 * Gets the id of the most recent station the bike was at
+	 * @return - most recent station ID
+	 */
 	public int getStatID() {
 		return statID;
 	}
 	
+	/**
+	 * Gets whether or not the bike is out on a ride
+	 * @return - true if checked out, false if at a station
+	 */
 	public boolean getOnRide() {
 		return onRide;
 	}
 	
+	
+	/**
+	 * Get whether the bike needs maintenance
+	 * @return - true if there is a maintenance request, false otherwise
+	 */
 	public boolean getMaintenance() {
 		return needsMaintenance;
 	}
@@ -45,6 +72,12 @@ public class bike {
 	 * Functional methods
 	 */
 	
+	
+	/**
+	 * Checks the bike into a station
+	 * @param statID - station to be checked into 
+	 * @return - true if everything went all right, false if the bike wasn't out on a ride or other error occurred
+	 */
 	public boolean checkIn(int statID) {
 		//TODO: Check bike into station. 
 		
@@ -57,6 +90,11 @@ public class bike {
 		
 	}
 	
+	
+	/**
+	 * Checks the bike out of a station
+	 * @return - true if no error, false if an error occurred
+	 */
 	public boolean checkOut() {
 		//TODO: Check bike out of station
 		
@@ -71,6 +109,10 @@ public class bike {
 		
 	}
 	
+	/**
+	 * Sets whether or not the bike needs maintenance
+	 * @param needsMaintenance - true for a maintenance request, false otherwise.
+	 */
 	public void setNeedsMaintenance(boolean needsMaintenance) {
 		this.needsMaintenance = needsMaintenance;
 		
