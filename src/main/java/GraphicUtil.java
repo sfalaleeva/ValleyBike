8482da263d9673b76c110c026a38c9aa78549dc8
LyraDusk <lauren@meyerzone.net>
2019-12-05 11:37:03 -0500

    Create GraphicUtil.java
    Added the graphic shell class, will work more seriously on it later today

---
+++ src/main/java/GraphicUtil.java
@@ -0,0 +1,16 @@
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/**
+ * A utility that controls the graphical representation of the bikes
+ * @author laurenmeyer
+ *
+ */
+public class GraphicUtil extends JPanel {
+	
+	
+	
+	public void paint(Graphics g) {
+		super.paint(g);
+	}
+}
